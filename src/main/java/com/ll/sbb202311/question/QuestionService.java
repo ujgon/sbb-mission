bb2fcb788a79e311b18884c88ef0e01afb73a570
Yoo <ujgon@naver.com>
2023-11-30 20:22:13 +0900

    2_10

--- src/main/java/com/ll/sbb202311/question/QuestionService.java
+++ src/main/java/com/ll/sbb202311/question/QuestionService.java
@@ -2,8 +2,9 @@ package com.ll.sbb202311.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import java.util.Optional;
+import com.ll.sbb202311.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +13,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
